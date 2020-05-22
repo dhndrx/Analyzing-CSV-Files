ca40dfb0a3276f23df1fb069fe62f560042005a7
Deontee Hendricks <48738169+dhndrx@users.noreply.github.com>
2020-05-22 03:32:26 -0500

    Update analyzingWeatherData.java

--- analyzingWeatherData.java
+++ analyzingWeatherData.java
@@ -7,6 +7,7 @@
  */
 
 import edu.duke.*;
+import java.io.*;
 import org.apache.commons.csv.*;
 
 public class Part1 {
@@ -38,5 +39,42 @@ public class Part1 {
         CSVRecord coldest = coldestHourInFile(fr.getCSVParser());
         System.out.println("Coldest temperature: " + coldest.get("TemperatureF")
                             +" at time:" + coldest.get("TimeEST"));
-    }    
+    }
+    
+    public String fileWithColdestTemperature()throws IOException{
+    
+        DirectoryResource dr = new DirectoryResource();
+        String coldestFile = "";
+        
+        for(File f : dr.selectedFiles()){
+        
+            FileResource fr = new FileResource(f.getCanonicalPath());
+            
+            CSVRecord currentRecord = coldestHourInFile(fr.getCSVParser());
+            
+            if(coldestFile.equals("")){
+                coldestFile = f.getCanonicalPath();
+            }
+            
+            else{
+                FileResource coldestFr = new FileResource(coldestFile);
+                CSVRecord coldestRecord = coldestHourInFile(coldestFr.getCSVParser());
+                double currentTemp = Double.parseDouble(currentRecord.get("TemperatureF"));
+                double coldestTemp = Double.parseDouble(coldestRecord.get("TemperatureF"));
+                if(currentTemp < coldestTemp && currentTemp != -9999){
+                    coldestFile = f.getCanonicalPath();
+                }
+            }
+        }
+        
+        return coldestFile;
+    }
+    
+    public void testFileWithColdestTemperature()throws IOException{
+    
+        String coldDay = fileWithColdestTemperature();
+        int found = coldDay.indexOf("weather-");
+        System.out.println(coldDay.substring(found));
+    
+    }
 }
